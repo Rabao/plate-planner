97bd86688efbce76598d6b7e88121ddf996dc8cc
Michael C <michaelwconde@gmail>
2022-05-11 19:14:25 -0500

    new commit

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,9 +1,13 @@
-import {Link} from 'react-router-dom'
+import {Switch, Route, Redirect, Link} from 'react-router-dom'
+import Recipes from './RecipesList';
 
 function Home(props) {
     return(
         <div>
-            You must be authorized to see this page.
+            <Link to='/recipes'>Recipes</Link>
+            <Switch>
+                <Route path='/recipes' component={() => <Recipes/>}/>
+            </Switch>
         </div>
     )
 }
