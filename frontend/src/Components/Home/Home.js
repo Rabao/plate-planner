ffdbcc054610563e2be764e618a0793900817cf3
Michael C <michaelwconde@gmail>
2022-05-11 19:22:55 -0500

    put switch route to main page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,13 +1,9 @@
 import {Switch, Route, Redirect, Link} from 'react-router-dom'
-import Recipes from './RecipesList';
 
 function Home(props) {
     return(
         <div>
             <Link to='/recipes'>Recipes</Link>
-            <Switch>
-                <Route path='/recipes' component={() => <Recipes/>}/>
-            </Switch>
         </div>
     )
 }
--- frontend/src/Components/Main/Main.js
+++ frontend/src/Components/Main/Main.js
@@ -3,6 +3,7 @@ import {Switch, Route, Redirect, Link} from 'react-router-dom'
 import Login from '../Login/Login'
 import Register from '../Register/Register'
 import Home from '../Home/Home'
+import Recipes from '../Home/RecipesList';
 import {addToken, deleteUser} from '../../Redux/actionCreators'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
@@ -45,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/login' component={() => <Login/>}/>
                     <Route path='/register'component={() => <Register/>}/>
+                    <Route path='/recipes' component={() => <Recipes/>}/>
                     <Route path='/home' component={this.props.token.token !== undefined ? () => <Home/> : null}/>
                     <Redirect to='/login'/>
                 </Switch>
