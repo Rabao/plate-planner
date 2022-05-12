2546d136f999adf815a334f546687218d28edd6b
Michael C <michaelwconde@gmail>
2022-05-11 19:05:44 -0500

    main page and recipes page

---
+++ frontend/src/Components/Home/RecipesList.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom'
+
+function Recipes(props) {
+    return(
+        <div>
+            This is the recipes page!
+        </div>
+    )
+}
+
+export default Recipes;
