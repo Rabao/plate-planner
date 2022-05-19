6f8bc766a68702bc377f7bc6ddbc7fbe79658274
Kadeam Howell <howell.a.k@gmail.com>
2022-05-19 07:25:37 -0400

    Merging.

---
+++ frontend/src/Components/SubComponents/DailyValue.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const DailyValue = () => {
+    return ( <
+        >
+        <
+        table >
+        <
+        tr >
+        <
+        td > Serving Size: < /td> <
+        td > Calories: < /td> <
+        td > Carbohydrates: < /td> <
+        td > Protein: < /td> <
+        td > Fat: < /td> <
+        /tr> <
+        tr >
+        <
+        td > Saturated Fat: < /td> <
+        td > Sodium: < /td> <
+        td > Fiber: < /td> <
+        td > Sugar: < /td> <
+        /tr> { CalculateValues } <
+        /table> <
+        />
+    );
+}
+
+//Calculator will take nutritional values and add them up, return them to the table above.
+function CalculateValues(props) {
+    console.log(props.ingredients);
+}
+
+export default DailyValue;
