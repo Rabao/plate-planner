3578ad114a054a0a78f9f856b3b56999d173793d
Michael C <michaelwconde@gmail>
2022-05-30 18:09:09 -0500

    favorite button implemented

---
+++ frontend/src/Redux/favorites.js
@@ -0,0 +1,39 @@
+import * as ActionTypes from "./ActionTypes";
+
+export const Favorites = (state = {
+        id: null,
+        isLoading: true,
+        errMess: null,
+        favorites: []
+    }, action) => {
+    
+    switch(action.type) {
+        case ActionTypes.ADD_FAVORITES:
+            return{...state,
+                id: action.payload.id, 
+                isLoading: false,
+                errMess: null,
+                favorites: action.payload}
+
+        case ActionTypes.ADD_FAVORITE:
+            return{...state, favorites: state.favorites.concat(action.payload)};
+        
+        case ActionTypes.DELETE_FAVORITES:
+            return { ...state, id: null, favorites: '', authorities: [] }
+
+        case ActionTypes.FAVORITES_LOADING:
+           return{...state,
+                isLoading: true,
+                errMess: null,
+                favorites: []}
+
+        case ActionTypes.FAVORITES_FAILED:
+            return{...state, 
+                isLoading: false,
+                errMess: action.payload,
+                favorites: []}
+
+        default:
+            return state;
+    }
+}
